76155cf808c960e9bc38d2dbde3367d8489159e4
Boris <borik1and@gmail.com>
2023-09-20 18:08:07 +0300

    add def test_str

--- tests/test_item.py
+++ tests/test_item.py
@@ -41,3 +41,8 @@ def test_repr():
     item1 = Item("Смартфон", 10000, 20)
     assert repr(item1) == "Item('Смартфон', 10000, 20)"
 
+
+def test_str():
+    item1 = Item("Смартфон", 10000, 20)
+    assert str(item1) == 'Смартфон'
+
