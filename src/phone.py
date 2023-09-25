6a6c65eacd016e1dfaa412e6121af6ab1d7c9c4d
boris <borik1and@gmail.com>
2023-09-25 14:39:25 +0300

    changing in def __init__

--- src/phone.py
+++ src/phone.py
@@ -2,8 +2,7 @@ from src.item import Item
 
 
 class Phone(Item):
-    def __init__(self, name: str, price: float, quantity: int, num_sim_card):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim):
         super().__init__(name, price, quantity)
-        self.num_sim_card = num_sim_card
-
+        self.number_of_sim = number_of_sim
 
