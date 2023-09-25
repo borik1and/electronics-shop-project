b7ced24ad13fbe3d50a529ce5135947551c07f1d
boris <borik1and@gmail.com>
2023-09-25 15:14:37 +0300

    del def __add__

--- src/phone.py
+++ src/phone.py
@@ -6,6 +6,5 @@ class Phone(Item):
         super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
 
-    def __add__(self, other):
-        # сложение по количеству товара в магазине
-        return self.quantity + other.quantity
+    def __repr__(self):
+        return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
