5791fc290c938b05bfced689bea50d1e82201770
boris <borik1and@gmail.com>
2023-09-25 15:13:39 +0300

    add def __add__

--- src/item.py
+++ src/item.py
@@ -28,6 +28,12 @@ class Item:
     def __str__(self):
         return self.__name
 
+    def __add__(self, other):
+        if issubclass(other.__class__, self.__class__):
+            return self.quantity + other.quantity
+        # сложение по количеству товара в магазине
+        return None
+
     @property
     def name(self) -> str:
         return self.__name
