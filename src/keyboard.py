a4bc05cf080b0c7c58916835c399c5b483775d3b
boris <borik1and@gmail.com>
2023-09-28 20:28:41 +0300

    changing in class Keyboard

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,19 +3,28 @@ from src.item import Item
 
 class LanguageMixin:
     def __init__(self):
-        self.__language = "EN"
+        self.language = "EN"
+
+    def change_lang(self):
+        if self.language == "EN":
+            self.language = "RU"
+        else:
+            self.language = "EN"
+
+
+
+class Keyboard(Item, LanguageMixin):
+    def __init__(self, name: str, price: float, quantity: int, language="EN"):
+        super().__init__(name, price, quantity)
+        self.__language = language
 
     @property
     def language(self):
         return self.__language
 
-    def change_lang(self):
-        if self.__language == "EN":
-            self.__language = "RU"
+    @language.setter
+    def language(self, value):
+        if value in ["EN", "RU"]:
+            self.__language = value  # Fix the attribute name here
         else:
-            self.__language = "EN"
-
-
-class Keyboard(Item, LanguageMixin):
-    def __init__(self, name: str, price: float, quantity: int):
-        super().__init__(name, price, quantity)
+            raise ValueError("AttributeError: property 'language' of 'Keyboard' object has no setter")
