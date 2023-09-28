dd2f66df02b418573cedacf4a972fefcc40c6a7f
boris <borik1and@gmail.com>
2023-09-28 16:53:26 +0300

    add def test_number_of_sim

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -11,3 +11,14 @@ def test_phone():
 def test_add():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert phone1 + phone1 == 10
+
+
+def test_number_of_sim():
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+
+    try:
+        phone1.number_of_sim = 0
+        # Если никаких исключений не делается, то испытание не проходит
+        assert False, "Ожидается ValueError"
+    except ValueError as e:
+        assert str(e) == "Количество физических SIM-карт должно быть целым числом больше нуля."
