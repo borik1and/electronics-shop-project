f3523a860bf6890d2557a5e7a8fe237b5404358a
boris <borik1and@gmail.com>
2023-09-28 16:39:28 +0300

    add def number_of_sim

--- src/phone.py
+++ src/phone.py
@@ -2,9 +2,20 @@ from src.item import Item
 
 
 class Phone(Item):
-    def __init__(self, name: str, price: float, quantity: int, number_of_sim):
+    def __init__(self, name: str, price: float, quantity, number_of_sim: int):
         super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
 
     def __repr__(self):
         return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    @property
+    def number_of_sim(self) -> int:
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, num: int) -> None:
+        # Check that the number of SIM cards is a positive integer
+        if not isinstance(num, int) or num < 1:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        self._number_of_sim = num
